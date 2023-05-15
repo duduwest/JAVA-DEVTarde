4f1d1eb01056a7345e379b52a9c2255198f8841e
duduwest <123770322+duduwest@users.noreply.github.com>
2023-05-15 19:09:31 +0000

    java

---
+++ POOintroducao/Pessoas.java
@@ -0,0 +1,19 @@
+package POOintroducao;
+
+public class Pessoas {
+    protected String nome;  
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    protected String endereco; 
+
+}
